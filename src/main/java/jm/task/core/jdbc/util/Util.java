32e22bdc25f25305180cb3f14e0aa19b6582cbd0
Maria <mashytkamickey@mail.ru>
2022-01-25 23:32:50 +0300

    описан класс UserHibernateDaoImpl

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -46,24 +46,7 @@ public class Util {
         }
         return null;
     }
-/*
-    public static void main(final String[] args) throws Exception {
-        final Session session = getSession();
-        try {
-            System.out.println("querying all the managed entities...");
-            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-            for (EntityType<?> entityType : metamodel.getEntities()) {
-                final String entityName = entityType.getName();
-                final Query query = session.createQuery("from " + entityName);
-                System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list()) {
-                    System.out.println("  " + o);
-                }
-            }
-        } finally {
-            session.close();
-        }
-    } */
+
 
 
 
